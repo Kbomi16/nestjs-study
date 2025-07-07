65a05ce27eec2378317a50d8a6a5d60312f69519
Kbomi16 <kimbomi2172@naver.com>
2025-07-07 21:24:04 +0900

    ✨ 기본 get, post 로직 구현

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common'
+import { BoardsService } from './boards.service'
+import { Board } from './board.model'
 
 @Controller('boards')
-export class BoardsController {}
+export class BoardsController {
+  constructor(private boardsService: BoardsService) {}
+
+  // 모든 게시물 가져오기
+  @Get('/')
+  getAllBoard(): Board[] {
+    return this.boardsService.getAllBoards()
+  }
+}
